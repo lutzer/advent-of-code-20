f6d13fbace5236b8bfc7998a04772cefdad64885
lutzer <in_the_fishtank@gmx.de>
2021-02-21 01:25:51 +0100

    added day19 solution1

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -7,7 +7,7 @@ enum Operator {
   Add, Multiply
 }
 
-fn calculate_instructions(input: &str, mut i: usize) -> (i64, usize) {
+fn calculate_instructions_1(input: &str, mut i: usize) -> (i64, usize) {
 
   let splits: Vec<&str> = input.split(" ").collect();
 
@@ -19,7 +19,7 @@ fn calculate_instructions(input: &str, mut i: usize) -> (i64, usize) {
       "+" => { operator = Operator::Add; },
       "*" => { operator = Operator::Multiply },
       "(" => {
-        let (r, skip_to) = calculate_instructions(input, i+1);
+        let (r, skip_to) = calculate_instructions_1(input, i+1);
         match operator {
           Operator::Add => {
             result += r;
@@ -50,17 +50,82 @@ fn calculate_instructions(input: &str, mut i: usize) -> (i64, usize) {
   return (result, i)
 }
 
+fn delimited_paranthesis(input: &[&str]) -> (usize, usize) {
+  let mut start = 0;
+  let mut opened = 0;
+  for (i,&c) in input.iter().enumerate() {
+    match c {
+      "(" => { opened +=1; start = i;  }
+      ")" => { opened -=1; if opened == 0 { return (start, i); } },
+      _ => { }
+    }
+  }
+  return (0,0);
+}
+
+fn calculate_instructions_2(input: &str) -> i64 {
+  let splits: Vec<&str> = input.split(" ").collect();
+
+  // let brackets = delimited_paranthesis(input);
+
+  let mut result: Vec<String> = vec![];
+  let mut i = 0;
+  while i < splits.len() {
+    match splits[i] {
+      "(" => {
+        let brackets = delimited_paranthesis(&splits[i..]);
+        result.push(splits[brackets.0+i+1..brackets.1+i].join(" "));
+        i += brackets.1;
+      },
+      _ => {
+        result.push(splits[i].to_string());
+       }
+    }
+    i += 1;
+  }
+
+  println!("{:?}:{:?}", splits, result);
+
+  // let mut operator = Operator::Add;
+  // let mut result = 0;
+  
+  // let mut mult_mem: Option<i64> = None;
+
+  // let mut i = 0;
+  // for (i,s) in splits.iter().enumerate() {
+  //   match s {
+  //     "+" => { operator = Operator::Add; },
+  //     "*" => { operator = Operator::Multiply },
+  //     _ => { }
+  //     } 
+  //   }
+  //   println!("{}:{:?} {}:{}", result, mult_mem, i, splits[i]);
+  //   i += 1;
+  // }
+  // let (s,e) = delimited_paranthesis(input);
+  // println!("{}:{}:{}", s, e, input);
+
+
+  // assert_eq!(parser("abc|efg"), Ok(("", "|")));
+  return 0;
+}
+
 fn part_1(input: &String) -> i64 {
   let results: Vec<i64> = input.lines().map(|l| { 
     let input_edited = l.replace("(", "( ").replace(")"," )");
-    let (result, _) = calculate_instructions(&input_edited, 0);
+    let (result, _) = calculate_instructions_1(&input_edited, 0);
     return result;
   }).collect();
   return results.iter().sum();
 }
 
 fn part_2(input: &String) -> i64 {
-  return 0
+  let results: Vec<i64> = input.lines().map(|l| { 
+    let input_edited = l.replace("(", "( ").replace(")"," )");
+    let result = calculate_instructions_2(&input_edited);
+    return result;
+  }).collect();
+  return results.iter().sum();
 }
 
 fn main() {
@@ -88,19 +153,20 @@ mod tests {
 
   #[test]
   fn test_1() {
-    assert_eq!(part_1(&"2 * 3 + 4".to_string()), 10);
-    assert_eq!(part_1(&"2 * 3 + (4 * 5)".to_string()), 26);
-    assert_eq!(part_1(&"5 + (8 * 3 + 9 + 3 * 4 * 3)".to_string()), 437);
-    assert_eq!(part_1(&"5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))".to_string()), 12240);
-    assert_eq!(part_1(&"((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2".to_string()), 13632);
+    // assert_eq!(part_1(&"2 * 3 + 4".to_string()), 10);
+    // assert_eq!(part_1(&"2 * 3 + (4 * 5)".to_string()), 26);
+    // assert_eq!(part_1(&"5 + (8 * 3 + 9 + 3 * 4 * 3)".to_string()), 437);
+    // assert_eq!(part_1(&"5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))".to_string()), 12240);
+    // assert_eq!(part_1(&"((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2".to_string()), 13632);
   }
 
   #[test]
   fn test_2() {
-    // assert_eq!(part_1(&"1 + (2 * 3) + (4 * (5 + 6))".to_string()), 51);
-    // assert_eq!(part_1(&"2 * 3 + (4 * 5)".to_string()), 46);
-    // assert_eq!(part_1(&"5 + (8 * 3 + 9 + 3 * 4 * 3)".to_string()), 1445);
-    // assert_eq!(part_1(&"5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))".to_string()), 669060);
-    // assert_eq!(part_1(&"((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2".to_string()), 23340);
+    assert_eq!(part_2(&"2 * 3 + 4".to_string()), 14);
+    //assert_eq!(part_2(&"1 + (2 * 3) + (4 * (5 + 6))".to_string()), 51);
+    // assert_eq!(part_2(&"2 * 3 + (4 * 5)".to_string()), 46);
+    // assert_eq!(part_2(&"5 + (8 * 3 + 9 + 3 * 4 * 3)".to_string()), 1445);
+    // assert_eq!(part_2(&"5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))".to_string()), 669060);
+    // assert_eq!(part_2(&"((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2".to_string()), 23340);
   }
 }
---
+++ day19/src/main.rs
@@ -0,0 +1,118 @@
+use std::fs;
+use clap::{Arg, App};
+use std::collections::HashMap;
+use str_macro::str;
+
+const FILENAME: &str = "input.txt";
+
+type RuleMap = std::collections::HashMap<String, std::vec::Vec<String>>;
+
+
+
+fn match_rule(message: &str, rule_key: String, rule_map: &RuleMap) -> (bool, usize) {
+  let rules = rule_map.get(&rule_key[..]).expect("Did not find rule");
+  let mut steps = 0;
+  let is_match = rules.iter().any(|rule| {
+    steps = 0;
+    if rule.starts_with("\"") {
+      steps += 1;
+      return message.starts_with(&rule.trim_matches('\"'));
+    } else {
+      return rule.split_whitespace().all(|c| {
+        let (is_match, skip) = match_rule(&message[steps..], c.to_string(), rule_map);
+        steps += skip;
+        return is_match;
+      });
+    }
+  });
+  return (is_match, steps);
+}
+
+fn part_1(input: &String) -> u64 {
+
+  let mut rules: RuleMap = input.split("\n\n").nth(0).unwrap().lines().fold(HashMap::new(),|mut acc, l| {
+    let splits: Vec<&str> = l.split(":").collect();
+    acc.insert(splits[0].to_string(), splits[1].split("|").map(|s| { s.trim().to_string() }).collect::<Vec<String>>());
+    return acc
+  });
+  // add end string
+  let messages: Vec<String> = input.split("\n\n").nth(1).unwrap().lines().map(|l| { [l,"$"].concat() }).collect();
+
+  // add end char $ to rules
+  rules.insert("$".to_string(),vec!["\"$\"".to_string()]);
+  *rules.get_mut(&str!("0")).unwrap() = rules.get(&str!("0")).unwrap().iter().map(|x| { format!("{} $",x) }).collect();
+
+  let result = messages.iter().filter(|m| {
+    let (is_match,_) = match_rule(m, "0".to_string(), &rules);
+    return is_match;
+  }).count();
+
+  return result as u64;
+}
+
+fn part_2(input: &String) -> u64 {
+  let mut rules: RuleMap = input.split("\n\n").nth(0).unwrap().lines().fold(HashMap::new(),|mut acc, l| {
+    let splits: Vec<&str> = l.split(":").collect();
+    acc.insert(splits[0].to_string(), splits[1].split("|").map(|s| { s.trim().to_string() }).collect::<Vec<String>>());
+    return acc
+  });
+  // add end string
+  let messages: Vec<String> = input.split("\n\n").nth(1).unwrap().lines().map(|l| { [l,"$"].concat() }).collect();
+
+  // add end char $ to rules
+  rules.insert(str!("$"),vec![str!("\"$\"")]);
+  *rules.get_mut(&str!("0")).unwrap() = rules.get(&str!("0")).unwrap().iter().map(|x| { format!("{} $",x) }).collect();
+
+  //change rule 8 and 42
+  *rules.get_mut(&str!("8")).unwrap() = vec![str!("42"), str!("42 8")];
+  *rules.get_mut(&str!("11")).unwrap() = vec![str!("42 31"), str!("42 11 31")];
+
+  let result = messages.iter().filter(|m| {
+    let (is_match,_) = match_rule(m, "0".to_string(), &rules);
+    return is_match;
+  }).count();
+
+  return result as u64;
+}
+
+fn main() {
+  let args = App::new("Advent of Code - Day 19")
+  .arg(Arg::with_name("part")
+  .takes_value(true)
+  .required(true))
+  .get_matches();
+  
+  let data = fs::read_to_string(FILENAME).expect("Input Error");
+
+  let part = args.value_of("part").unwrap_or("");
+  let result = match part {
+    "1" => { part_1(&data.trim_end().to_string()) },
+    "2" => { part_2(&data.trim_end().to_string()) },
+    _ => { panic!("Select either part 1 or 2") }
+  };
+  println!("Result is: {}", result);
+}
+
+// run with cargo test -- --nocapture to show print output
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_part1() {
+    let data = fs::read_to_string("test-input.txt").expect("Input Error");
+    let result = part_1(&data.trim_end().to_string());
+    assert_eq!(result, 2);
+  }
+
+  #[test]
+  fn test2() {
+    // let input = String::from(indoc! {"
+    //   .#.
+    //   ..#
+    //   ###
+    // "});
+    // let result = part_2(&input);
+    // assert_eq!(result, 848);
+  }
+}
